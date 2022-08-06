a06d350bbd244d7edf933a10cab56487411a74b5
Samuel Dantas <dantasamuca@gmail.com>
2022-08-07 01:49:16 +0200

    add code parser

--- parser.py
+++ parser.py
@@ -27,8 +27,48 @@ class CodeLine:
                 "You must fetch the indentation before the dedentation"
             )
 
-        if previous_code_line.indents > self.indents:
+        if (
+            previous_code_line.indents is not None
+            and previous_code_line.indents > self.indents
+        ):
             self.dedents = previous_code_line.indents - self.indents
+
+    def __str__(self):
+        """
+        Logict to pretty print the code and the indentation and dedentation
+        """
+        break_line = ''
+        if self.indents == 0 or (self.indents == self.dedents):
+            break_line = '\n'
+
+        return (
+            f'{break_line}'
+            f'{self.raw_line} ==> '
+            f'(indents: {self.indents or 0}, dedents: {self.dedents or 0})'
+        )
+
+
+class Parser:
+    def __init__(self, raw_code: str):
+        self.raw_code = raw_code
+
+    @property
+    def lines(self):
+        lines = []
+        raw_lines = [i for i in self.raw_code.split('\n') if i]
+
+        for i, line in enumerate(raw_lines):
+            if i == 0:
+                code_line = CodeLine(line)
+                code_line.fetch_indentation()
+            else:
+                code_line = CodeLine(line)
+                code_line.fetch_indentation()
+                code_line.fetch_dedentation(lines[i - 1])
+
+            lines.append(code_line)
+
+        return lines
 
 
 def get_definition(line: str) -> str:
@@ -58,29 +98,40 @@ def get_definition(line: str) -> str:
 
 
 code = """
-def test_function():
-    return 'bla'
+import os
 
-class Z:
-    pass
+from collections import namedtuple
 
-class A(Z):
-    def b(self, a):
-        return a + 1
+from .config import ASTERISK_FILES
 
-    @classmethod
-    def _c(cls):
-        pass
 
-    def somemethod_with_conditions(self):
-        if True:
-            return 'SOMETHING'
-        else:
-            return 'ANOTHER'
+class AsteriskFile:
+    def __init__(self, query, file):
+        self._query = query
+        self._file = file
 
-        return 'SOMETHING ELSE'
+    # TODO: I let the content accessible if we want to run some test on it
+    # we could validade the content with a more 'integration test' like
+    @property
+    def content(self):
+        return self._file.template.render(query=self._query)
+
+    def save(self):
+        with open(self._file.path, 'w') as file:
+            file.write(self.content)
+
+
+class Writer:
+    @staticmethod
+    def generate(type, query):
+        assert type in ASTERISK_FILES
+
+        file = ASTERISK_FILES[type]
+
+        return AsteriskFile(query=query, file=file)
 """
 
 
-for i in code.split('\n'):
-    print(get_definition(i), '-->', i)
+parser = Parser(code)
+for i in parser.lines:
+    print(i)
